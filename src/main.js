7bcd1009e3299324b46214aa2f3b987d773556f0
smz <1056046456.com>
2022-04-30 15:43:04 +0800

    多功能面板

--- src/main.js
+++ src/main.js
@@ -3,6 +3,6 @@ import App from './App.vue'
 
 // 加载路由配置
 import router from './router/index' 
-createApp(App)
+createApp(App)      // 是通过 createApp 来执行 Vue 的初始化，另外不管是 Vue 生态里的东西，还是外部插件、 UI 框架，统一都是由 use 来激活初始化
     .use(router)
     .mount('#app')
